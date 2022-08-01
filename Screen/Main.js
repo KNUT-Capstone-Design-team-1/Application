daaf2fd0644ae2225af617d1526a13bec7e9e8e8
Jintae <jintea519@gmail.com>
2022-08-02 00:33:05 +0900

    Fix : nearby pharmacies update slow

--- Screen/Main.js
+++ Screen/Main.js
@@ -4,9 +4,6 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-// 현재 위치 파악을 위한 라이브러리
-import RNLocation from 'react-native-location';
-
 // 화면 구성을 위한 컴포넌트
 import {
   SafeAreaView,
@@ -19,47 +16,12 @@ import {
 // 앱 내에서 사용되는 전역변수
 global.img_uri = ''; // 화면에 표시되는 이미지의 주소
 global.img_base64 = ''; // json에 첨부될 base64
-global.place = ''; // kakao map API에서 받아오는 약국 정보
 global.p_data = ''; // 알약에 대한 데이터
 global.pill_managing_sw = 0; // Pill Storage에서 저장 버튼과 삭제 버튼을 전환하는 스위치
-
-// 주변 약국 정보를 수집하기 위한 위치정보 최신화 및 카카오맵 API 호출
-async function get_pharm_list() {
-  // 위치 정보 최신화
-  RNLocation.subscribeToLocationUpdates();
-  // 마지막 위치정보 좌표를 카카오맵 API로 전송
-  RNLocation.getLatestLocation({timeout: 60000}).then(async latestLocation => {
-    try {
-      // latestLocation.longitude   latestLocation.latitude
-      let response = await fetch(
-        'https://dapi.kakao.com/v2/local/search/category.json?category_group_code=PM9&radius=20000&x=' +
-          latestLocation.longitude +
-          '&y=' +
-          latestLocation.latitude +
-          '&input_coord=WGS84',
-        {
-          headers: {Authorization: 'KakaoAK 33a8b02db1a0de6d37b4d7de43955e46'},
-        },
-      );
-      // 카카오맵으로 부터 응답받은 데이터를 json으로 파싱
-      const result_tmp = await response.json();
-      // 약국의 이름과 정보 url만 추출하여 리스트에 저장
-      place = result_tmp.documents.map(res => ({
-        name: res.place_name,
-        url: res.place_url,
-      }));
-    } catch (e) {
-      console.log(e);
-    }
-  });
-}
 
 export default function Main(props) {
   // 화면 이동을 위한 네비게이션 선언
   const {navigation} = props;
-
-  // 카카오 맵 API 호출을 수행하는 함수 호출
-  get_pharm_list();
 
   return (
     <SafeAreaView style={styles.container}>
--- Screen/Nearby_Pharmacies.js
+++ Screen/Nearby_Pharmacies.js
@@ -1,7 +1,10 @@
-import * as React from 'react';
+import React, {useCallback, useState} from 'react';
 // 화면 이동을 위한 라이브러리
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useFocusEffect} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
+// 현재 위치 파악을 위한 라이브러리
+import Geolocation from 'react-native-geolocation-service';
 
 //반응형 크기 조절
 import {RFValue} from 'react-native-responsive-fontsize';
@@ -20,8 +23,47 @@ import {
 global.pharm_url = '';
 
 export default function Nearby_Pharmacies(props) {
+  const [pharmMap, setPharmMap] = useState();
+
   // 화면 이동을 위한 네비게이션 선언
   const {navigation} = props;
+
+  useFocusEffect(
+    useCallback(() => {
+      const getPharmList = async () => {
+        // 마지막 위치정보 좌표를 카카오맵 API로 전송
+        Geolocation.getCurrentPosition(async position => {
+          try {
+            // position.longitude   position.latitude
+            let response = await fetch(
+              'https://dapi.kakao.com/v2/local/search/category.json?category_group_code=PM9&radius=500&x=' +
+                position.coords.longitude +
+                '&y=' +
+                position.coords.latitude +
+                '&input_coord=WGS84',
+              {
+                headers: {
+                  Authorization: 'KakaoAK 33a8b02db1a0de6d37b4d7de43955e46',
+                },
+              },
+            );
+            // 카카오맵으로 부터 응답받은 데이터를 json으로 파싱
+            const result_tmp = await response.json();
+
+            // 약국의 이름과 정보 url만 추출하여 리스트에 저장
+            const pharms = result_tmp.documents.map(res => ({
+              name: res.place_name,
+              url: res.place_url,
+            }));
+            setPharmMap(pharms);
+          } catch (e) {
+            console.log(e);
+          }
+        });
+      };
+      getPharmList();
+    }, []),
+  );
 
   // kakao map api으로 부터 받아온 약국 이름을 표시하기 위한 Flat List 렌더링 <스크롤>
   const render_list = ({item}) => (
@@ -53,7 +95,7 @@ export default function Nearby_Pharmacies(props) {
 
       {/* 약국 목록을 표시하기 위한 Flat List */}
       <FlatList
-        data={place}
+        data={pharmMap}
         renderItem={render_list}
         keyExtractor={(item, index) => index.toString()}
       />
